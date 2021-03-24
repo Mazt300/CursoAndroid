2fe1a8369773388b45b8d08ae10bf75e40d97ba4
MSCorp <programador07@mscorp.group>
2021-03-24 14:59:37 -0600

    Inicio Curso Android Nativo

---
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proyecto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonNavigationActivity);
+
+//        Definir evento click sobre el boton de login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NotasActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
